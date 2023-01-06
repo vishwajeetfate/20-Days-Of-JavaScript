0e32dbaab9e3970d93204b4fcf04c6a3d2973871
vishwajeet <110755446+vishwajeetfate@users.noreply.github.com>
2023-01-06 19:53:54 +0530

    Add files via upload

---
+++ 04-Day/Loops/Do-While.js
@@ -0,0 +1,10 @@
+var n1 = 0;
+            
+document.write("Starting Loop" + "<br />");
+do {
+   document.write("Current Count : " + n1 + "<br />");
+  n1++;
+}
+
+while (n1 < 5);
+document.write ("Loop stopped!");
---
+++ 04-Day/Loops/Whlie.js
@@ -0,0 +1,9 @@
+var count = 0;
+document.write("Starting Loop ");
+
+while (count < 10) {
+   document.write("Current Count : " + count + "<br />");
+   count++;
+}
+
+document.write("Loop stopped!");
